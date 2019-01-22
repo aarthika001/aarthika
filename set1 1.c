eb9b720f9690bba6bcbfd020c3eaa6e9ed617125
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-01-22 16:23:00 +0530

    Create set1 1.c

---
+++ set1 1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+scanf("%d",&a);
+if(a>0)
+{
+printf("positive");
+}
+else if(a<0)
+{
+printf("negative");
+}
+else
+{
+printf("default");
+}
+getch();
+}
