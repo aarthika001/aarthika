157ea5bc33c472816b91d5ce8c7b5b95c408da35
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-01-29 14:10:27 +0530

    Create set2.c

---
+++ set2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[10];
+scanf("%s",&a);
+printf("%s",a);
+if(a=='sunday')
+printf("yes");
+else if(a=='monday')
+printf("no");
+else if(a=='tuesday')
+printf("no");
+else if(a=='wednesday')
+printf("no");
+else if(a=='thrusday')
+printf("no");
+else if(a=='friday')
+printf("no");
+else if(a=='saturday')
+printf("no");
+else
+printf("default");
+getch();
+}
