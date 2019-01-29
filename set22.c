c6f70ecd6ec912873e16984687cddfdb3f5441db
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-01-29 14:40:50 +0530

    Create set22.c

---
+++ set22.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+char a;
+scanf("%c",&a);
+if(a=='a'||a=='e'||a=='o'||a=='i'||a=='u')
+{
+printf("vowel");
+}
+else
+{
+    printf("consonant");
+}
+
+getch();
+return 0;
+}
