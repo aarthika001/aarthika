15a2c835396edca3ad6fcb9d0ce8b76927de753c
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-02-11 15:52:09 +0530

    Create beg9.c

---
+++ beg9.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,a[10],k,i,s=0;
+clrscr();
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<k;i++)
+{
+for(j=0;j<n;j++)
+{
+s=s+i;}
+}
+printf("%d",s);
+getch();
+}
