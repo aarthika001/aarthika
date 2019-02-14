d75c274cc7c4b46596362586cba2203a82abc843
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-02-14 21:40:38 +0530

    Create Beg61.c

---
+++ Beg61.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[10];
+int k,i;
+clrscr();
+scanf("%s%d",&a,&k);
+for(i=0;a[i]!='\0';i++)
+{
+l++;
+}
+if(k<=l)
+{
+for(i=0;i<k;i++)
+{
+printf("%c",a[i]);
+}}
+getch();
+}
+
