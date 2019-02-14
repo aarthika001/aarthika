b374b0359cbf4f509f22b91a688696e54a41f0fd
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-02-14 21:07:01 +0530

    Create Beg36.c

---
+++ Beg36.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[10];
+int i,c=0;
+clrscr();
+scanf("%[^\n]",&a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]=='.'||a[i]=='&'||a[i]=='#')
+c++;
+}
+printf("%d",c);
+getch();
+}
