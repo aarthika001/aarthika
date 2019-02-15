d449457eaf74a82869a141c8538f13a8da0c9ec5
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-02-15 19:21:34 +0530

    Create Beg21.c

---
+++ Beg21.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,d,j,b[10],s=0;
+    scanf("%d%d%d",&n,&a,&d);
+    for(j=0;j<n;j++)
+    {
+        b[i]=a+i*d;
+        sum=sum+b[j];
+    }
+    printf("%d",s);
+    return 0;
+}
