08330d40e717e898692c937dc189407dbcc8fffa
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-02-15 19:24:41 +0530

    Create Beg22.c

---
+++ Beg22.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int m,c[10],i,b=0;
+    scanf("%d",&m);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&c[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(c[i]>b)
+            b=c[i];
+    }
+        printf("%d",b);
+    
+return 0;
+}
