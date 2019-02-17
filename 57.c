45c65d81ea01dc6d9c7bc95b424eb59bcf532c81
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-02-17 14:06:55 +0530

    Create 57.c

---
+++ 57.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int p[10],i,m,r,count=0;
+clrscr();
+scanf("%d%d",&m,&r);
+for(i=0;i<m;i++)
+{
+scanf("%d",&p[i]);
+}
+for(i=0;i<m;i++)
+{
+if(p[i]==r)
+count++;
+}
+printf("%d",l);
+getch();
+}
