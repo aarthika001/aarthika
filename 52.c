51cd20d1d8729aa3a904c6891a8381330921adbd
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-02-17 09:28:39 +0530

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+clrscr();
+scanf("%d",&i);
+if(i==1)
+printf("one");
+else if(i==2)
+printf("two");
+else if(i==3)
+printf("three");
+else if(i==4)
+printf("four");
+else if(i==5)
+printf("five");
+else if(i==6)
+printf("six");
+else if(i==7)
+printf("seven");
+else if(i==8)
+printf("eight");
+else if(i==9)
+printf("nine");
+else if(i==10)
+printf("ten");
+else
+printf("invalid");
+getch();
+}
