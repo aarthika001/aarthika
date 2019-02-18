dbda7ea7c36ce1d474ede7412267da57861b7e1a
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-02-18 19:56:35 +0530

    Create Pla21.c

---
+++ Pla21.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+void main()
+{ 
+int i1,i2,i3,j1,j2,j3; 
+scanf("%d %d %d %d %d %d",&i1,&j1,&i2,&j2,&i3,&j3); if(i1==0&&i2==0 &&i3==0) printf("yes"); 
+else if(j1==0&&j2==0&&j3==0) printf("yes"); 
+else 
+printf("no"); 
+ 
+}
