6899436fd0a6b9ad89cde051ed04339fe0571d3b
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-02-19 15:11:29 +0530

    Create player13.c

---
+++ player13.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,rem,square=0;
+clrscr();
+scanf("%d",&n);
+while(n)
+{
+rem=n%10;
+square=square+rem*rem;
+n=n/10;
+}
+printf("%d",s);
+
+}
