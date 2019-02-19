eb6bf03a408ad7e39ecdc9f444154aa2a5018d61
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-02-19 16:19:30 +0530

    Create pla32.c

---
+++ pla32.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+int a[10],n,k,i,f=0;
+scanf("%d%d",&n,&k);
+for(i=0;i<n;i++)
+{
+    scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+    if(a[i]==k)
+    {
+    f=1;
+    break;
+    }
+}
+if(f==1)
+printf("yes");
+else
+printf("not");
+    return 0;
+}
