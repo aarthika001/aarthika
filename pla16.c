f612e4d9ffd676cc8253a0d35dcd9bf80549122f
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-02-19 15:29:57 +0530

    Create pla16.c

---
+++ pla16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int n,i,j,c=0,a[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                c++;
+            }
+        }
+        if(c==1)
+        printf("%d",a[i]);
+    }
+    return 0;
+}
