7b06a7956a2212cfd9609ad3d35f6ef447a49e05
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-02-22 15:57:52 +0530

    Create pla56.c

---
+++ pla56.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+   char a[10],k;
+   int i;
+   scanf("%s %c",&a,&k);
+   for(i=0;a[i]!='\0';i++)
+   {
+       if(k==a[i])
+       {
+           printf("%d",i+1);
+           break;
+       }
+   }
+    return 0;
+}
