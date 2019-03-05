5c32514cb921a434052735b26986ecc23b06c880
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-03-05 20:33:06 +0530

    Create pla75.c

---
+++ pla75.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main()
+{
+    int a[100],i,j,m,l=0;
+    scanf("%d",&m);
+    for(i=0;i<m;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<m;i++)
+    {   
+        for(j=i+1;j<m;j++)
+        {
+            if(a[i]<a[j])
+            {
+                l++;
+            
+            }
+        }
+    }
+   printf("%d",l);
+    
+}
