a499605f7d16aec147fdcd33e39190a1fb2f33d4
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-03-05 19:06:04 +0530

    Create pla61.c

---
+++ pla61.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void main()
+{
+ int n,m,i,j,x[50],t=0,l=0;
+ scanf("%d",&n);
+ scanf("%d",&m);
+ for(i=0;i<n;i++)
+ {
+     scanf("%d",&x[i]);
+ }
+ for(i=0;i<n;i++)
+ {
+    for(j=i+1;j<n;j++)
+    {
+      t=x[i]+x[j];
+      if(t==m)
+      {
+        l=1;
+        break;
+      }
+    }
+    if(l==1)
+        break;
+    
+ }
+    if(l==1)
+    
+      printf("yes");
+     
+    else
+    
+      printf("no");
+     
+    
+}
