80db5877de523d66ce6a1f138ea82cc21e4582fe
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-03-05 19:55:24 +0530

    Create pla66.c

---
+++ pla66.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int i,N,K,a[10],i,l;
+scanf("%d%d",&N,&k);
+for(i=0;i<N;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<N;i++)
+{
+if(a[i]==K)
+{
+l++;
+}
+}
+printf("%d",l);
+}
