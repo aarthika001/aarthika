3ed48190d94473c3f0df6ff18d4d4ae189297acc
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-03-05 19:09:01 +0530

    Create pla62.c

---
+++ pla62.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+ int a,m,i;
+ scanf("%d",&a);
+ for(i=1;i<a;i++)
+ {
+     if(a%i==0)
+     {
+        m=a/i;
+        if(m%2==1)
+        {
+            printf("%d",i);
+            break;
+        }
+     }
+ }
+}
