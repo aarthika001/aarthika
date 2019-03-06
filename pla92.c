5fb93042ad07541543dc5e0402f2bdda155dab46
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-03-06 20:18:40 +0530

    Create pla92.c

---
+++ pla92.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,d=0,base=1,rem;
+scanf("%d",&a);
+while(a)
+{
+rem=a%10;
+d=d+rem*base;
+a=a/10;
+base=base*2;
+}
+printf("%d",d);
+getch();
+}
