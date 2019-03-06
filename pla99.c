608f2743e58b58fb06686208e8887a9188e643de
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-03-06 21:02:22 +0530

    Create pla99.c

---
+++ pla99.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,o=0,base=1,rem;
+scanf("%d",&a);
+while(a)
+{
+rem=a%8;
+o=o+rem*base;
+a=a/8;
+base=base*2;
+}
+printf("%d",o);
+getch();
+}
