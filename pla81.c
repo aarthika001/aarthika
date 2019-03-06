faf53343278c668bf3a43ce66ddb1fb6c040fd91
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-03-06 20:00:52 +0530

    Create pla81.c

---
+++ pla81.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    int m,a[100],i,j,n,x=0;
+    scanf("%d",&m);
+    for(i=0;i<m;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            n=(a[i])&(a[j]);
+            if(n>x)
+            {
+                x=n;
+            }
+        }
+    }
+    printf("%d",x);
+   
+}
