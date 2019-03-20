a7215fda8a61431cbf2240c62c9b407174e4c0d3
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-03-20 11:47:06 +0530

    Create pla130.c

---
+++ pla130.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int n,a[10000],s=0,i,j;
+   scanf("%d",&N);
+   for(i=0;i<n;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+   for(i=0;i<n;i++)
+   {
+   s=s+a[i];
+   if(s%2==0)
+   {
+   printf("%d ",s);
+   } 
+   else
+   { 
+   printf("%d ",s);
+   }    
+}
+
+}
