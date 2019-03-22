ef4042cd126078f92ad0e02d8953ea54593de92d
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-03-22 11:30:14 +0530

    Create pla135.c

---
+++ pla135.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,n,a[100],l,t,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    l=n/2;
+    for(i=0;i<l;i++)
+    {
+     for(j=i+1;j<l;j++)   
+     {
+        if(a[i]>a[j])
+        {
+            t=a[i];
+            a[i]=a[j];
+            a[j]=t;
+        }
+     }
+    }
+    for(i=l;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]<a[j])
+            {
+                t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
