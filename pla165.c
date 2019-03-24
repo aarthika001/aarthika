d95f21e374404b8a7afd366f2426efad51e7d115
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-03-24 22:01:13 +0530

    Create pla165.c

---
+++ pla165.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a[100],i,n,k,f=0,c;
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    c=a[0];
+    for(i=0;i<n;i++)
+    {
+    
+         if((a[i]>c)&&(a[i]>k))
+         {
+             printf("%d",a[i]);
+             break;
+         }
+         else
+         {
+             c=a[i];
+         }
+    }}
+    
+        
