254a0787d986b4c8d16b8bef95e5de2a66ddb368
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-03-25 20:42:05 +0530

    Create pla172.c

---
+++ pla172.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+   int n,i,r=0,a[100],k=0,t=0,j;
+   scanf("%d",&n);
+   while(n)
+   {
+       r=n%10;
+       a[k]=r;
+       k++;
+       n=n/10;
+   }
+   for(i=0;i<k;i++)
+   {
+       for(j=i+1;j<k;j++)
+       {
+       if(a[i]<a[j])
+       {
+       t=a[i];
+       a[i]=a[j];
+       a[j]=t;
+       }
+       }
+       }
+       for(i=0;i<k;i++)
+     {  
+       printf("%d",a[i]);
+   }
+   }
