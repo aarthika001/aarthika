3a0437cfafd87f4bd32ba4c73075481e8b6c63eb
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-03-26 06:18:21 +0530

    Update pla188.c

--- pla188.c
+++ pla188.c
@@ -2,10 +2,14 @@
 #include<conio.h>
 void main()
 {
-    int a,b,c,s=0;
-    scanf("%d %d %d",&a,&b,&c);
-    if((a<b+c)||(b<c+a)||(c<a+b))
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if((a<b+c)&&(b<c+a)&&(c<a+b))
+    {
     printf("yes");
+    }
     else
+    {
     printf("no");
+    }
 }
