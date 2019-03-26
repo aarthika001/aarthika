328e6fcac2822d7562b683fdd3c5cee42c24cc5f
aarthika001 <46884912+aarthika001@users.noreply.github.com>
2019-03-26 06:16:33 +0530

    Create pla188.c

---
+++ pla188.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c,s=0;
+    scanf("%d %d %d",&a,&b,&c);
+    if((a<b+c)||(b<c+a)||(c<a+b))
+    printf("yes");
+    else
+    printf("no");
+}
